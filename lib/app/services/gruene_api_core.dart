d08c17bde692b4f25df92b142df63702a6ad92e0
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-01-10 11:54:18 +0100

    565: Move getFromApi to api_core

--- lib/app/services/gruene_api_core.dart
+++ lib/app/services/gruene_api_core.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 
 import 'package:chopper/chopper.dart';
+import 'package:chopper/chopper.dart';
+import 'package:get_it/get_it.dart';
 import 'package:gruene_app/app/constants/config.dart';
 import 'package:gruene_app/app/services/access_token_authenticator.dart';
 import 'package:gruene_app/app/services/interceptors/auth_interceptor.dart';
 import 'package:gruene_app/app/services/interceptors/keep_alive_interceptor.dart';
 import 'package:gruene_app/app/services/interceptors/user_agent_interceptor.dart';
+import 'package:gruene_app/i18n/translations.g.dart';
 import 'package:gruene_app/swagger_generated_code/gruene_api.swagger.dart';
 
 Future<GrueneApi> createGrueneApiClient() async {
@@ -20,4 +23,20 @@ Future<GrueneApi> createGrueneApiClient() async {
     authenticator: Config.grueneApiAccessToken == null ? AccessTokenAuthenticator() : null,
     interceptors: interceptors,
   );
+}
+
+Future<T> getFromApi<S, T>({
+  required Future<Response<S>> Function(GrueneApi api) request,
+  required T Function(S data) map,
+}) async {
+  final GrueneApi api = GetIt.I<GrueneApi>();
+
+  final response = await request(api);
+  final body = response.body;
+
+  if (!response.isSuccessful || body == null) {
+    throw Exception('${t.error.apiError(statusCode: response.statusCode)}\n${response.base.request?.url}');
+  }
+
+  return map(body);
 }
