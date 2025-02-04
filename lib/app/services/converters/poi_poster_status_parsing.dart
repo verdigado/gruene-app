c8ddd5f8476128175be756a9b59ecfc42537dedc
Christian Fiebrig <fiebrig@bitidee.de>
2025-02-04 11:21:48 +0100

    546: new poster status "to be moved"

--- lib/app/services/converters/poi_poster_status_parsing.dart
+++ lib/app/services/converters/poi_poster_status_parsing.dart
@@ -7,6 +7,7 @@ extension PoiPosterStatusParsing on PoiPosterStatus {
       PoiPosterStatus.damaged => PosterStatus.damaged,
       PoiPosterStatus.missing => PosterStatus.missing,
       PoiPosterStatus.removed => PosterStatus.removed,
+      PoiPosterStatus.toBeMoved => PosterStatus.to_be_moved,
       PoiPosterStatus.swaggerGeneratedUnknown => throw UnimplementedError(),
     };
   }
@@ -17,6 +18,7 @@ extension PoiPosterStatusParsing on PoiPosterStatus {
       PoiPosterStatus.damaged => t.campaigns.poster.status.damaged.label,
       PoiPosterStatus.removed => t.campaigns.poster.status.removed.label,
       PoiPosterStatus.missing => t.campaigns.poster.status.missing.label,
+      PoiPosterStatus.toBeMoved => t.campaigns.poster.status.to_be_moved.label,
       PoiPosterStatus.swaggerGeneratedUnknown => throw UnimplementedError(),
     };
   }
