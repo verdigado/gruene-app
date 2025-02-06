9bc695835bdd3767990440e71040c7dcd2778452
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-02-06 13:02:20 +0100

    212: Add search bar

---
+++ lib/app/widgets/search_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:gruene_app/app/theme/theme.dart';
+import 'package:gruene_app/i18n/translations.g.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  final void Function(String selected) setQuery;
+  final String query;
+
+  const CustomSearchBar({super.key, required this.setQuery, required this.query});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return SearchBar(
+      onChanged: setQuery,
+      leading: Icon(Icons.search_outlined, color: ThemeColors.textDisabled),
+      hintText: t.common.search,
+      hintStyle: WidgetStatePropertyAll(const TextStyle(color: ThemeColors.textDisabled)),
+      trailing: query.isNotEmpty
+          ? [IconButton(onPressed: () => setQuery(''), icon: Icon(Icons.clear, color: ThemeColors.textDisabled))]
+          : [],
+      backgroundColor: WidgetStatePropertyAll(theme.colorScheme.surface),
+      padding: WidgetStatePropertyAll(EdgeInsets.only(left: 8)),
+      shape: WidgetStatePropertyAll(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+          side: BorderSide(color: ThemeColors.textDisabled),
+        ),
+      ),
+      elevation: WidgetStatePropertyAll(0),
+    );
+  }
+}
