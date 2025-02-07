b0b4eb4e251357ae42a1689aad68d0d598e41cf7
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-02-07 12:20:38 +0100

    212: Fix search focus and clearing button

--- lib/app/widgets/search_bar.dart
+++ lib/app/widgets/search_bar.dart
@@ -3,23 +3,40 @@ import 'package:gruene_app/app/theme/theme.dart';
 import 'package:gruene_app/app/utils/debouncer.dart';
 import 'package:gruene_app/i18n/translations.g.dart';
 
-class CustomSearchBar extends StatelessWidget {
+class CustomSearchBar extends StatefulWidget {
   final void Function(String selected) setQuery;
   final String query;
 
   const CustomSearchBar({super.key, required this.setQuery, required this.query});
 
   @override
+  State<CustomSearchBar> createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  final Debouncer _debouncer = Debouncer();
+  final _controller = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final debouncer = Debouncer();
     return SearchBar(
-      onChanged: (query) => debouncer.run(() => setQuery(query)),
+      controller: _controller,
+      onTapOutside: (_) => FocusManager.instance.primaryFocus?.unfocus(),
+      onChanged: (query) => _debouncer.run(() => widget.setQuery(query)),
       leading: Icon(Icons.search_outlined, color: ThemeColors.textDisabled),
       hintText: t.common.search,
       hintStyle: WidgetStatePropertyAll(const TextStyle(color: ThemeColors.textDisabled)),
-      trailing: query.isNotEmpty
-          ? [IconButton(onPressed: () => setQuery(''), icon: Icon(Icons.clear, color: ThemeColors.textDisabled))]
+      trailing: widget.query.isNotEmpty
+          ? [
+              IconButton(
+                onPressed: () {
+                  _controller.clear();
+                  widget.setQuery('');
+                },
+                icon: Icon(Icons.clear, color: ThemeColors.textDisabled),
+              ),
+            ]
           : [],
       backgroundColor: WidgetStatePropertyAll(theme.colorScheme.surface),
       padding: WidgetStatePropertyAll(EdgeInsets.only(left: 8)),
