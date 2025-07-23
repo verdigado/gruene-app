bf4292ae5afd3074f75730ffc44f1b97c39f200c
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-07-23 10:33:07 +0200

    234: Link to release notes on version number

--- lib/app/constants/urls.dart
+++ lib/app/constants/urls.dart
@@ -5,3 +5,4 @@ const supportUrl = 'https://www.gruene.de/unterstuetzen';
 const mfaSettingsUrl = 'https://saml.gruene.de/realms/gruenes-netz/account/account-security/signing-in';
 const mfaInformationUrl = 'https://gruenstreifen.netzbegruenung.de/anmelden-im-gruenen-netz/anleitung-2fa-via-app';
 const grueneAppFeedbackUrl = 'https://gruenede.typeform.com/to/ADq4RxII';
+const releaseNotesUrl = 'https://github.com/verdigado/gruene-app/releases';
