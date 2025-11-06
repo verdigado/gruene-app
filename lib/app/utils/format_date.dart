140caea1e895ed082a7b2abdd2b7e9d7e2076ac5
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-11-06 13:05:30 +0200

    722: Move date time formatting to utils

--- lib/app/utils/format_date.dart
+++ lib/app/utils/format_date.dart
@@ -1,7 +1,17 @@
 import 'package:intl/intl.dart';
 
 const dateFormat = 'dd.MM.yyyy';
+const timeFormat = 'HH:mm';
+const dateTimeFormat = '$dateFormat, $timeFormat';
 
-String formatDate(DateTime date) {
-  return DateFormat(dateFormat).format(date);
-}
+String timeSuffix() => Intl.getCurrentLocale().startsWith('de') ? ' Uhr' : '';
+
+String formatDate(DateTime date) => DateFormat(dateFormat).format(date);
+
+String formatTime(DateTime time) => '${DateFormat(timeFormat).format(time)}${timeSuffix()}';
+
+String formatDateTime(DateTime dateTime) => '${DateFormat(dateTimeFormat).format(dateTime)}${timeSuffix()}';
+
+String formatInterval(String start, String end) => '$start - $end';
+
+String formatMonth(DateTime month) => DateFormat.yMMMM().format(month);
