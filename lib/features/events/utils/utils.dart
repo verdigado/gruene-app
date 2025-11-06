23f44421238babfae2844dc971bc61e1ea7e3c30
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-11-06 13:57:55 +0200

    722: Hide past events and past months

--- lib/features/events/utils/utils.dart
+++ lib/features/events/utils/utils.dart
@@ -5,14 +5,17 @@ import 'package:intl/intl.dart';
 
 List<MonthGroup> groupEventsByMonth(List<CalendarEvent> events) {
   final Map<DateTime, List<CalendarEvent>> groupedMap = {};
+  final now = DateTime.now();
 
-  for (var event in events) {
-    final monthKey = DateTime(event.start.year, event.start.month);
-    groupedMap.putIfAbsent(monthKey, () => []).add(event);
+  for (final event in events) {
+    final startMonth = DateTime(event.start.year, event.start.month);
+    final currentMonth = DateTime(now.year, now.month);
+    final month = startMonth.isBefore(currentMonth) ? currentMonth : startMonth;
+    groupedMap.putIfAbsent(month, () => []).add(event);
   }
 
-  final List<MonthGroup> groupedList = groupedMap.entries.map((entry) {
-    entry.value.sort((a, b) => a.start.compareTo(b.start));
+  final groupedList = groupedMap.entries.map((entry) {
+    entry.value.sort((a, b) => (a.start == b.start) ? a.title.compareTo(b.title) : a.start.compareTo(b.start));
     return MonthGroup(month: entry.key, events: entry.value);
   }).toList();
 
