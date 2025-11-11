bd63b230fa922c351c49b37d892f3d026b2b9402
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-11-11 14:00:11 +0200

    722: Add filters for events

--- lib/features/events/screens/events_screen.dart
+++ lib/features/events/screens/events_screen.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:gruene_app/app/models/filter_model.dart';
 import 'package:gruene_app/app/screens/future_loading_screen.dart';
+import 'package:gruene_app/app/utils/date.dart';
 import 'package:gruene_app/app/widgets/app_bar.dart';
+import 'package:gruene_app/app/widgets/filter_bar.dart';
 import 'package:gruene_app/features/events/domain/events_api_service.dart';
+import 'package:gruene_app/features/events/repository/events_repository.dart';
+import 'package:gruene_app/features/events/utils/utils.dart';
+import 'package:gruene_app/features/events/widgets/events_filter_dialog.dart';
 import 'package:gruene_app/features/events/widgets/events_list.dart';
 import 'package:gruene_app/features/events/widgets/events_map.dart';
 import 'package:gruene_app/i18n/translations.g.dart';
+import 'package:gruene_app/swagger_generated_code/gruene_api.swagger.dart';
+
+class EventsScreenContainer extends StatelessWidget {
+  const EventsScreenContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MainAppBar(title: t.events.events),
+      body: FutureLoadingScreen(
+        load: () async => (await getEvents(), await readCalendarFilterKeys()),
+        buildChild: (params, _) {
+          final ((events, calendars), calendarFilterKeys) = params;
+          final List<Calendar> initialCalendarFilters = calendarFilterKeys == null
+              ? calendars
+              : calendars.where((calendar) => calendarFilterKeys.contains(calendar.id)).nonNulls.toList();
+          return EventsScreen(events: events, calendars: calendars, initialCalendarFilters: initialCalendarFilters);
+        },
+      ),
+    );
+  }
+}
 
 class EventsScreen extends StatefulWidget {
-  const EventsScreen({super.key});
+  final List<CalendarEvent> events;
+  final List<Calendar> calendars;
+  final List<Calendar> initialCalendarFilters;
+
+  const EventsScreen({super.key, required this.initialCalendarFilters, required this.events, required this.calendars});
 
   @override
   State<EventsScreen> createState() => _EventsScreenState();
@@ -15,45 +47,76 @@ class EventsScreen extends StatefulWidget {
 
 class _EventsScreenState extends State<EventsScreen> {
   bool isMapView = false;
+  String _query = '';
+  late List<Calendar> _selectedCalendars;
+  DateTimeRange? _dateRange;
+  final DateTimeRange defaultDateRange = DateTimeRange(start: startOfDay(), end: dateInfinity());
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedCalendars = widget.initialCalendarFilters;
+    _dateRange = defaultDateRange;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: MainAppBar(title: t.events.events),
-      body: FutureLoadingScreen(
-        load: getEvents,
-        buildChild: (data, _) {
-          final now = DateTime.now();
-          final today = DateTime(now.year, now.month, now.day);
-          // Hide past events (only show events that start or end today or in the future)
-          final relevantEvents = data
-              .where((event) => event.end == null ? event.start.isAfter(today) : event.end!.isAfter(today))
-              .toList();
-          return Stack(
-            children: [
-              Positioned.fill(
-                child: isMapView ? EventsMap(events: relevantEvents) : EventsList(events: relevantEvents),
-              ),
-              Positioned(
-                bottom: 8,
-                left: 0,
-                right: 0,
-                child: Center(
-                  child: SegmentedButton(
-                    segments: [
-                      ButtonSegment(value: false, icon: const Icon(Icons.list), label: Text(t.events.list)),
-                      ButtonSegment(value: true, icon: const Icon(Icons.map), label: Text(t.events.map)),
+    final events = widget.events.filter(_selectedCalendars, _dateRange);
+
+    final searchFilter = FilterModel(update: (query) => setState(() => _query = query), initial: '', selected: _query);
+    final calendarFilter = FilterModel(
+      update: (calendars) => setState(() => _selectedCalendars = calendars),
+      initial: widget.calendars,
+      selected: _selectedCalendars,
+      values: widget.calendars,
+    );
+    final dateRangeFilter = FilterModel(
+      update: (dateRange) => setState(() => _dateRange = dateRange),
+      initial: defaultDateRange,
+      selected: _dateRange,
+    );
+
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: isMapView
+              ? EventsMap(events: events)
+              : Container(
+                  padding: EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 64),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      FilterBar(
+                        searchFilter: searchFilter,
+                        modified: [calendarFilter, dateRangeFilter].modified(),
+                        filterDialog: EventsFilterDialog(
+                          calendarFilter: calendarFilter,
+                          dateRangeFilter: dateRangeFilter,
+                        ),
+                      ),
+                      Expanded(child: EventsList(events: events)),
                     ],
-                    selected: {isMapView},
-                    onSelectionChanged: (newSelection) => setState(() => isMapView = newSelection.first),
-                    showSelectedIcon: false,
                   ),
                 ),
-              ),
-            ],
-          );
-        },
-      ),
+        ),
+        Positioned(
+          bottom: 8,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: SegmentedButton(
+              segments: [
+                ButtonSegment(value: false, icon: const Icon(Icons.list), label: Text(t.events.list)),
+                ButtonSegment(value: true, icon: const Icon(Icons.map), label: Text(t.events.map)),
+              ],
+              selected: {isMapView},
+              onSelectionChanged: (newSelection) => setState(() => isMapView = newSelection.first),
+              showSelectedIcon: false,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/features/events/widgets/events_filter_dialog.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:gruene_app/app/models/filter_model.dart';
+import 'package:gruene_app/app/widgets/date_range_filter.dart';
+import 'package:gruene_app/app/widgets/filter_dialog.dart';
+import 'package:gruene_app/app/widgets/selection_view.dart';
+import 'package:gruene_app/features/events/repository/events_repository.dart';
+import 'package:gruene_app/i18n/translations.g.dart';
+import 'package:gruene_app/swagger_generated_code/gruene_api.swagger.dart';
+
+class EventsFilterDialog extends StatefulWidget {
+  final FilterModel<List<Calendar>> calendarFilter;
+  final FilterModel<DateTimeRange?> dateRangeFilter;
+
+  const EventsFilterDialog({
+    super.key,
+    required this.calendarFilter,
+    required this.dateRangeFilter,
+  });
+
+  @override
+  State<EventsFilterDialog> createState() => _EventsFilterDialogState();
+}
+
+// showFullScreenDialog creates a new BuildContext, such that state updates in the parent do not update widgets in the dialog
+// We therefore need a local copy to reflect the state changes here as well
+class _EventsFilterDialogState extends State<EventsFilterDialog> {
+  late List<Calendar> _localSelectedCalendars;
+  late DateTimeRange? _localDateRange;
+
+  @override
+  void initState() {
+    super.initState();
+    _localSelectedCalendars = widget.calendarFilter.selected;
+    _localDateRange = widget.dateRangeFilter.selected;
+  }
+
+  void setCalendars(List<Calendar> calendars) {
+    widget.calendarFilter.update(calendars);
+    setState(() => _localSelectedCalendars = calendars);
+    writeCalendarFilterKeys(calendars);
+  }
+
+  void resetFilters() {
+    setCalendars(widget.calendarFilter.initial);
+    widget.dateRangeFilter.reset();
+    setState(() => _localDateRange = widget.dateRangeFilter.initial);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final calendars = widget.calendarFilter.values;
+
+    final filtersModified = widget.calendarFilter.modified(_localSelectedCalendars) || widget.dateRangeFilter.modified(_localDateRange);
+
+    return FilterDialog(
+      resetFilters: resetFilters,
+      modified: filtersModified,
+      children: [
+        SelectionView(
+          setSelectedOptions: setCalendars,
+          title: t.events.calendars,
+          options: calendars,
+          selectedOptions: _localSelectedCalendars,
+          getLabel: (calendar) => calendar.displayName,
+        ),
+        DateRangeFilter(
+          title: t.events.dateRange,
+          dateRange: _localDateRange,
+          setDateRange: (dateRange) {
+            setState(() => _localDateRange = dateRange);
+            widget.dateRangeFilter.update(dateRange);
+          },
+        ),
+      ],
+    );
+  }
+}
