0734086e738fd214a0bd87ee210236f64c90c8d5
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-11-18 10:50:06 +0100

    722: Add categories filter and event creation dialog

--- lib/features/events/constants/index.dart
+++ lib/features/events/constants/index.dart
@@ -1,2 +1,6 @@
 const eventsSourceName = 'events-source';
 const eventsLayerName = 'events-layer';
+
+const prominentEventCategories = ['Sitzung', 'Öffentlich'];
+const moreEventCategories = ['Kennenlernen', 'Parteitag', 'Wahlkampf', 'Workshop'];
+const eventCategories = [...prominentEventCategories, ...moreEventCategories];
