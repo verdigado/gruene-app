007508b04209734f7635ec3ea24849f36eb05921
Christian Fiebrig <fiebrig@bitidee.de>
2025-11-28 13:10:06 +0100

    735 archiving team (#790)
    * 733: refactor teams screen
    
    - use spinner when loading user info
    - create_team logic in mixin for separation of concern
    
    * 733: show team profile
    
    * 300: leave team
    
    - refactor some parts of ConfirmDeleteMixin to cope better with some wider use
    
    * fix lint checks
    
    * 735 archiving team
    
    * 736: show team assignments (#793)

--- lib/features/campaigns/screens/teams/team_home.dart
+++ lib/features/campaigns/screens/teams/team_home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gruene_app/app/auth/repository/user_info.dart';
+import 'package:gruene_app/app/services/converters.dart';
 import 'package:gruene_app/app/theme/theme.dart';
 import 'package:gruene_app/features/campaigns/screens/mixins.dart';
+import 'package:gruene_app/features/campaigns/screens/teams/team_assigned_elements.dart';
 import 'package:gruene_app/features/campaigns/screens/teams/team_profile.dart';
 import 'package:gruene_app/i18n/translations.g.dart';
 import 'package:gruene_app/swagger_generated_code/gruene_api.swagger.dart';
@@ -31,7 +33,7 @@ class _TeamHomeState extends State<TeamHome> with ConfirmDelete {
   void _loadData() async {
     setState(() => _loading = true);
 
-    // TODO remove mock data when getOwnTeam is working
+    // TODO #733 remove mock data when getOwnTeam is working
     // var teamsService = GetIt.I<GrueneApiTeamsService>();
     // var team = await teamsService.getOwnTeam();
     var team = Team(
@@ -73,6 +75,8 @@ Chat: https://signal.group/#123456''',
     return Column(
       children: [
         TeamProfile(currentTeam: _currentTeam!, currentUser: widget.currentUser),
+        TeamAssignedElements(currentTeam: _currentTeam!),
+
         GestureDetector(
           onTap: _leaveTeam,
           child: Container(
@@ -94,6 +98,30 @@ Chat: https://signal.group/#123456''',
             ),
           ),
         ),
+        _currentTeam!.isTeamLead(widget.currentUser)
+            ? GestureDetector(
+                onTap: _archiveTeam,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(width: 0.5, color: ThemeColors.textLight)),
+                  ),
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(t.campaigns.team.archive_team, style: theme.textTheme.bodyLarge),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Icon(Icons.chevron_right, color: theme.textTheme.bodyLarge?.color),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            : SizedBox.shrink(),
+        SizedBox(height: 24),
       ],
     );
   }
@@ -110,6 +138,22 @@ Chat: https://signal.group/#123456''',
       onDeletePressed: executeLeaveTeam,
       title: '${t.campaigns.team.leave_team}?',
       confirmationDialogText: t.campaigns.team.leave_team_confirmation_dialog,
+      actionTitle: t.common.actions.confirm,
+    );
+  }
+
+  void _archiveTeam() {
+    void executeArchiveTeam() {
+      // TODO #735 use archive functionality on API
+      // var teamsService = GetIt.I<GrueneApiTeamsService>();
+      // teamsService.deleteTeam(_currentTeam.id);
+    }
+
+    confirmDelete(
+      context,
+      onDeletePressed: executeArchiveTeam,
+      title: '${t.campaigns.team.archive_team}?',
+      confirmationDialogText: t.campaigns.team.archive_team_confirmation_dialog,
       actionTitle: t.common.actions.confirm,
     );
   }
