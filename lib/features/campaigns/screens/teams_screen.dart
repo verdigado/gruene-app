2db925d81f9c948a6842f86a6a7e073c4e7d4f17
Christian Fiebrig <fiebrig@bitidee.de>
2025-12-09 10:52:15 +0100

    297: allow to pull the team screen down to reload

--- lib/features/campaigns/screens/teams_screen.dart
+++ lib/features/campaigns/screens/teams_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:gruene_app/app/services/converters.dart';
 import 'package:gruene_app/app/services/gruene_api_user_service.dart';
+import 'package:gruene_app/app/theme/theme.dart';
 import 'package:gruene_app/features/campaigns/screens/teams/new_team_mixin.dart';
 import 'package:gruene_app/features/campaigns/screens/teams/team_home.dart';
 import 'package:gruene_app/swagger_generated_code/gruene_api.swagger.dart';
@@ -45,7 +46,14 @@ class _TeamsScreenState extends State<TeamsScreen> with NewTeamMixin {
     rows.add(TeamHome(currentUser: _currentUserInfo));
     rows.add(_currentUserInfo.isCampaignManager() ? getNewTeamButton(context) : SizedBox.shrink());
 
-    return SingleChildScrollView(child: Column(children: rows));
+    return RefreshIndicator(
+      color: ThemeColors.primary,
+      backgroundColor: ThemeColors.sun,
+      onRefresh: () {
+        return Future.delayed(Duration.zero, reload);
+      },
+      child: SingleChildScrollView(child: Column(children: rows)),
+    );
   }
 
   @override
