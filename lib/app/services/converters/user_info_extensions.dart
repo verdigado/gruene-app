8c07a208f6ac576ad375f0fbe5c6895f77ca92cd
Niko Heller <hellerniko@gmail.com>
2026-01-02 13:06:51 +0100

    878: add sherpa roles to permit campaign features

--- lib/app/services/converters/user_info_extensions.dart
+++ lib/app/services/converters/user_info_extensions.dart
@@ -1,17 +1,36 @@
 part of '../converters.dart';
 
 extension UserInfoExtension on UserRbacStructure {
-  static const campaignManagerKV = '7555238'; // Grünes Netz - Wahlatlas KV - Wahlkampfkoordinatorin
-  static const campaignManagerLV = '7555237'; // Grünes Netz - Wahlatlas LV - Wahlkampfkoordinatorin
-  static const campaignManagerBV = '7555236'; // Grünes Netz - Wahlatlas BV - Wahlkampfkoordinatorin
+
+  static const campaignManagerRoles = [
+    // Wahlatlas
+    '7555238', // Grünes Netz - Wahlatlas KV - Wahlkampfkoordinatorin
+    '7555237', // Grünes Netz - Wahlatlas LV - Wahlkampfkoordinatorin
+    '7555236', // Grünes Netz - Wahlatlas BV - Wahlkampfkoordinatorin
+    // Wahlkampfkoordinator
+    '5891385', // Landesverband GR - Koordinatorin Bundestagswahlkampf
+    '7362861', // Landesverband GR - Koordinatorin Europawahlkampf
+    '584949', // Landesverband GR - Koordinatorin Landtagswahlkampf
+    '6849163', // Landesverband GR - Koordinatorin Kommunalwahlkampf
+    '5878787', // Kreisverband GR - Koordinatorin Bundestagswahlkampf
+    '7362866', // Kreisverband GR - Koordinatorin Europawahlkampf
+    '585131', // Kreisverband GR - Koordinatorin Landtagswahlkampf
+    '5878781', // Kreisverband GR - Koordinatorin Kommunalwahlkampf
+    // Lokalkoordinator
+    '9551523', // Haustür-WK-Lokal-Koordination Bundestagswahl
+    '13474682', // Haustür-WK-Lokal-Koordination Europawahl
+    '11798625', // Haustür-WK-Lokal-Koordination Landtagswahl
+    '11798626', // Haustür-WK-Lokal-Koordination Landtagswahl
+    '11798677', // Haustür-WK-Lokal-Koordination Kommunalwahl
+  ];
 
   bool isCampaignManager() {
-    return roles.map((r) => r.id).any((r) => [campaignManagerBV, campaignManagerKV, campaignManagerLV].contains(r));
+    return roles.map((r) => r.id).any((r) => campaignManagerRoles.contains(r));
   }
 
   bool isCampaignManagerInDivision(String division) {
     return roles
-        .where((r) => [campaignManagerBV, campaignManagerKV, campaignManagerLV].contains(r.id))
+        .where((r) => campaignManagerRoles.contains(r.id))
         .any((r) => division.startsWith(r.divisionKey.stripRight('0')));
   }
 }
