05931dad7070cada6bff875468197af1be91cabb
Steffen Kleinle <steffen.kleinle@mailbox.org>
2026-03-30 10:55:31 +0200

    988: Improve no events error screen

--- lib/app/screens/error_screen.dart
+++ lib/app/screens/error_screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:gruene_app/app/theme/theme.dart';
 import 'package:gruene_app/app/utils/error_message.dart';
 import 'package:gruene_app/i18n/translations.g.dart';
 
 class ErrorScreen<T> extends StatelessWidget {
   final Object? error;
   final String? errorMessage;
+  final IconData? icon;
   final T Function() retry;
 
-  const ErrorScreen({super.key, required this.retry, this.error, this.errorMessage})
+  const ErrorScreen({super.key, required this.retry, this.error, this.errorMessage, this.icon})
     : assert((error == null) != (errorMessage == null) && error is! String);
 
   @override
@@ -16,10 +18,20 @@ class ErrorScreen<T> extends StatelessWidget {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 16,
         children: [
+          Icon(icon, color: ThemeColors.grey200, size: 48),
           Text(errorMessage ?? getErrorMessage(error!), textAlign: TextAlign.center, style: theme.textTheme.bodyLarge),
-          SizedBox(height: 16),
-          ElevatedButton(onPressed: retry, child: Text(t.error.retry)),
+          TextButton(
+            onPressed: retry,
+            style: ButtonStyle(
+              foregroundColor: WidgetStatePropertyAll(ThemeColors.text),
+              textStyle: WidgetStatePropertyAll(
+                theme.textTheme.labelLarge!.apply(decoration: TextDecoration.underline),
+              ),
+            ),
+            child: Text(t.error.retry),
+          ),
         ],
       ),
     );
