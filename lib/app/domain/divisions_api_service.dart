d93a18a52d002ce94933ab38d866deafd217c6f6
Steffen Kleinle <steffen.kleinle@mailbox.org>
2026-06-30 13:52:34 +0200

    221: Show all party divisions in profile

--- lib/app/domain/divisions_api_service.dart
+++ lib/app/domain/divisions_api_service.dart
@@ -1,5 +1,7 @@
 import 'package:gruene_app/app/services/gruene_api_core.dart';
 import 'package:gruene_app/swagger_generated_code/gruene_api.swagger.dart';
 
-Future<List<Division>> loadDivisions() async =>
-    getFromApi(request: (api) => api.v1DivisionsGet(limit: 100000), map: (data) => data.data);
+Future<List<Division>> loadDivisions([List<String>? divisionKeys]) async => getFromApi(
+  request: (api) => api.v1DivisionsGet(limit: 100000, divisionKey: divisionKeys),
+  map: (data) => data.data,
+);
